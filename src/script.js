f2baa736ec1eebbbf6f1ea8656814993161b7734
Tri <trimpham98@gmail.com>
2023-07-02 23:14:28 -0700

    Apply wind force to blow ball back after launching

--- src/script.js
+++ src/script.js
@@ -189,6 +189,8 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update physics world
+  sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
+
   world.step(1 / 60, deltaTime, 3);
 
   sphere.position.copy(sphereBody.position);
