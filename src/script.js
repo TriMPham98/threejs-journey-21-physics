5a958667d88a0babe85be1e6c24e2fcab4124375
Tri <trimpham98@gmail.com>
2023-07-03 07:06:04 -0700

    Create function to create three.js and cannon.js sphere

--- src/script.js
+++ src/script.js
@@ -149,6 +149,36 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Utils
+ */
+
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(radius, 20, 20),
+    new THREE.MeshStandardMaterial({
+      metalness: 0.3,
+      roughness: 0.4,
+      envMap: environmentMapTexture,
+    })
+  );
+  mesh.castShadow = true;
+  mesh.position.copy(position),
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius);
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial
+  })
+  body.position.copy(position);
+  world.addBody(body);
+};
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
