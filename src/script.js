6bf9c4a12dc9c13073c1d6ee558996000f5833bc
Tri <trimpham98@gmail.com>
2023-07-05 15:36:35 -0700

    Add box rotation using quaternion

--- src/script.js
+++ src/script.js
@@ -261,6 +261,7 @@ const tick = () => {
 
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
 
   // Update controls
