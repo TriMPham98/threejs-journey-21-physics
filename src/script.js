e08485ba7133ece859b61fbf0694a6f95ef4540d
Tri <trimpham98@gmail.com>
2023-07-07 07:32:47 -0700

    Randomize volume on impact for realistic sound

--- src/script.js
+++ src/script.js
@@ -50,6 +50,7 @@ const playHitSound = (collision) => {
   const impactStrength = collision.contact.getImpactVelocityAlongNormal();
 
   if (impactStrength > 1.5) {
+    hitSound.volume = Math.random();
     hitSound.currentTime = 0;
     hitSound.play();
   }
