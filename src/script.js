cb9f607a9ada797c6c98951dd0631251bcbf62b6
Tri <trimpham98@gmail.com>
2023-07-07 07:39:22 -0700

    Remove sound, object body, and mesh in reset function

--- src/script.js
+++ src/script.js
@@ -32,7 +32,16 @@ debugObject.createBox = () => {
 gui.add(debugObject, "createBox");
 
 debugObject.reset = () => {
-  console.log("reset");
+  for (const object of objectsToUpdate) {
+    // Remove body
+    object.body.removeEventListener("collide", playHitSound);
+    world.removeBody(object.body);
+
+    // Remove mesh
+    scene.remove(object.mesh);
+  }
+
+  objectsToUpdate.splice(0, objectsToUpdate.length);
 };
 
 gui.add(debugObject, "reset");
